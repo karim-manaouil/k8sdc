cb5de0333aa4a7a0c59ae67ed9a66f23864db767
Karim MANAOUIL <afr0ck@afr0ck.inria>
2020-04-28 08:27:06 +0200

    per-component request percentages

--- util/quick.sh
+++ util/quick.sh
@@ -27,10 +27,39 @@ generate_hdb_for_comps(){
     done
 }
 
+print_percn_table(){
+    printf "percn\t\t master/k8s\t\t workers/k8s\t\t kubelets/workers\n"
+
+    for i in 0 25 50 250 400; do
+        cluster_n=$(cat hdb/$i/cluster/all.json | \
+            jq '.data.result[] | select(.metric.le | contains("+Inf")) | .values[]' | \
+            tail -2 | head -1 | sed 's/"//g' | xargs) 
+        master_n=$(cat hdb/$i/master/all.json | \
+            jq '.data.result[] | select(.metric.le | contains("+Inf")) | .values[]' | \
+            tail -2 | head -1 | sed 's/"//g' | xargs)
+        workers_n=$(cat hdb/$i/workers/all.json | \
+            jq '.data.result[] | select(.metric.le | contains("+Inf")) | .values[]' | \
+            tail -2 | head -1 | sed 's/"//g' | xargs)
+        kubelets_n=$(cat hdb/$i/kubelets/all.json | \
+            jq '.data.result[] | select(.metric.le | contains("+Inf")) | .values[]' | \
+            tail -2 | head -1 | sed 's/"//g' | xargs)
+    
+    n1=$(echo $master_n/$cluster_n*100 | bc -l)
+    n2=$(echo $workers_n/$cluster_n*100 | bc -l) 
+    n3=$(echo $kubelets_n/$workers_n*100 | bc -l)
+
+    printf "%dms\t\t %f\t\t %f\t\t %f\n" $i $n1 $n2 $n3 
+done
+
+
+}
+
+
 if [[ $# -ne 1 ]]; then
     echo "Provide \$1 please"
     exit 1
 fi
 
-generate_hdb_for_comps $1
-
+print_percn_table && echo
+#generate_hdb_for_comps $1
+    
